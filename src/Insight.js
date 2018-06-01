6c41739cadc2d577d6bf986582ce689d81e706b6
Sky Young <sky@alexandria.io>
2018-06-01 16:48:10 -0700

    Return data from response

--- src/Insight.js
+++ src/Insight.js
@@ -7,20 +7,17 @@ class Insight {
 		this.url = url;
 
 		this.api = axios.create({
-			baseURL: this.url,
-			transformResponse: [function (data) {
-				return data.data;
-			}]
+			baseURL: this.url
 		})
 	}
 	getBlock(hash){
-		return this.api.get("/block/" + hash)
+		return this.api.get("/block/" + hash).then((res) => { return res.data })
 	}
 	getBlockIndex(height){
-		return this.api.get("/block-index/" + height)
+		return this.api.get("/block-index/" + height).then((res) => { return res.data })
 	}
 	getRawBlock(block){
-		return this.api.get("/rawblock/" + block)
+		return this.api.get("/rawblock/" + block).then((res) => { return res.data })
 	}
 	getBlockSummary(limit, blockDate){
 		var reqURL = "/blocks";
@@ -28,6 +25,7 @@ class Insight {
 
 		if (limit && limit !== ""){
 			reqURL += "?limit=" + limit;
+			addedQuestionMark = true;
 		}
 
 		if (blockDate && blockDate !== ""){
@@ -41,13 +39,13 @@ class Insight {
 			reqURL += "blockDate=" + blockDate;
 		}
 
-		return this.api.get(reqURL)
+		return this.api.get(reqURL).then((res) => { return res.data })
 	}
 	getTransaction(txid){
-		return this.api.get("/tx/" + txid)
+		return this.api.get("/tx/" + txid).then((res) => { return res.data })
 	}
 	getRawTransaction(txid){
-		return this.api.get("/rawtx/" + txid)
+		return this.api.get("/rawtx/" + txid).then((res) => { return res.data })
 	}
 	getAddress(address, options){
 		var reqURL = "/addr/" + address;
@@ -80,43 +78,43 @@ class Insight {
 			}
 		}
 
-		return this.api.get(reqURL)
+		return this.api.get(reqURL).then((res) => { return res.data })
 	}
 	getAddressProperties(address, property){
-		return this.api.get("/addr/" + address + "/" + property)
+		return this.api.get("/addr/" + address + "/" + property).then((res) => { return res.data })
 	}
 	getAddressUtxo(address){
-		return this.api.get("/addr/" + address + "/utxo")
+		return this.api.get("/addr/" + address + "/utxo").then((res) => { return res.data })
 	}
 	getAddressesUtxo(addresses){
-		return this.api.post("/addrs/utxo", {addrs: addresses.join()})
+		return this.api.post("/addrs/utxo", {addrs: addresses.join()}).then((res) => { return res.data })
 	}
 	getTransactionsForBlock(hash){
-		return this.api.get("/txs/?block=" + hash)
+		return this.api.get("/txs/?block=" + hash).then((res) => { return res.data })
 	}
 	getTransactionsForAddress(address){
-		return this.api.get("/txs/?address=" + address)
+		return this.api.get("/txs/?address=" + address).then((res) => { return res.data })
 	}
 	getTransactionsForAddresses(addresses, options){
-		var opts = options;
+		var opts = options || {};
 		opts.addrs = addresses.join();
 
-		return this.api.post("/addrs/txs", opts)
+		return this.api.post("/addrs/txs", opts).then((res) => { return res.data })
 	}
 	broadcastRawTransaction(rawtx, options){
-		var opts = options;
+		var opts = options || {};
 		opts.rawtx = rawtx;
 
-		return this.api.post("/tx/send", opts)
+		return this.api.post("/tx/send", opts).then((res) => { return res.data })
 	}
 	getSync(){
-		return this.api.get("/sync")
+		return this.api.get("/sync").then((res) => { return res.data })
 	}
 	getPeer(){
-		return this.api.get("/peer")
+		return this.api.get("/peer").then((res) => { return res.data })
 	}
 	getStatus(query){
-		return this.api.get("/status?q=" + query)
+		return this.api.get("/status?q=" + query).then((res) => { return res.data })
 	}
 	estimateFee(nbBlocks){
 		var reqURL = "/utils/estimatefee";
@@ -124,7 +122,7 @@ class Insight {
 		if (nbBlocks && nbBlocks !== "")
 			reqURL += "?nbBlocks=" + nbBlocks
 
-		return this.api.get(reqURL)
+		return this.api.get(reqURL).then((res) => { return res.data })
 	}
 	on(event, callback){
 		if (!this.socket){
